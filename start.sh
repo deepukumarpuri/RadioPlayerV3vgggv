5cc78f289091cae3e5ee16e8bb46f391f13f4a2b
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-09-03 17:13:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone https://github.com/deepukumarpuri/rlrad.git /RadioPlayerV3
+git clone https://github.com/deepukumarpuri/rlrad.git /rlrad
 cd /RadioPlayerV3
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
